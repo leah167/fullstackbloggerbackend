c6759b9c45029dc2389445cc97d98dbccd4c797b
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-07-08 09:28:00 -0600

    completed thursday's assignment

--- routes/blogs.js
+++ routes/blogs.js
@@ -8,8 +8,27 @@ router.get("/hello-blogs", (req, res) => {
 
 router.get("/all-blogs", async function (req, res) {
   try {
+    const limit = Number(req.query.limit);
+    const skip = Number(req.query.limit) * (Number(req.query.page) - 1);
+    const sortField = req.query.sortField;
+    const sortOrder = req.query.sortOrder === "ASC" ? 1 : -1;
+    const filterField = req.query.filterField;
+    const filterValue = req.query.filterValue;
     const collection = await blogsDB().collection("blogs50");
-    const posts = await collection.find({}).toArray();
+    let filterObj = {};
+    if (filterField && filterValue) {
+      filterObj = { [filterField]: filterValue };
+    }
+    let sortObj = {};
+    if (sortField && sortOrder) {
+      sortObj = { [sortField]: sortOrder };
+    }
+    const posts = await collection
+      .find(filterObj)
+      .sort(sortObj)
+      .limit(limit)
+      .skip(skip)
+      .toArray();
     res.json({ message: posts });
   } catch (e) {
     res.status(500).send("Error fetching posts.");
